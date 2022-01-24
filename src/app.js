21f20c87599b4d75e6d61388f1f2d498ab3b3dd3
Clay Hamilton <clayham@gmail.com>
2022-01-24 15:39:32 -0800

    -learned import/export with webpack using ES6 syntax -installed validator package with yarn

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("app.js is running!!!");
+//imported files run before app.js:
+// import "./utils.js";
+import sub, {square, add} from "./utils.js";
+
+console.log("app.js is running!");
+console.log(square(4));
+console.log(add(3,4));
+console.log(sub(23,4));
+
+import isSenior, {isAdult, canDrink} from "./person.js";
+console.log(isAdult(1));
+console.log(canDrink(20));
+console.log(isSenior(66));
