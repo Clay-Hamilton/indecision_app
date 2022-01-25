64fb05b019f29ddb2d4742f5fb6072d11c646438
Clay Hamilton <clayham@gmail.com>
2022-01-24 16:14:40 -0800

    -added babel-loader to webpack -everything runs through webpack now -TODO: split React components into individual files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 //imported files run before app.js:
-// import "./utils.js";
-import sub, {square, add} from "./utils.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log("app.js is running!");
-console.log(square(4));
-console.log(add(3,4));
-console.log(sub(23,4));
-
-import isSenior, {isAdult, canDrink} from "./person.js";
-console.log(isAdult(1));
-console.log(canDrink(20));
-console.log(isSenior(66));
+const template = <p>REAL JSX FROM WEBPACK</p>;
+ReactDOM.render(template, document.getElementById("app"));
