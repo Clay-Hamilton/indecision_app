fe323a2ad38b8f06dc0e53ade582ea136fbc5b2a
Clay Hamilton <clayham@gmail.com>
2022-01-26 15:29:27 -0800

    -converted event handlers to arrow functions-eliminates need for constructors

--- src/app.js
+++ src/app.js
@@ -5,3 +5,27 @@ import IndecisionApp from "./components/IndecisionApp";
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+
+class OldSyntax {
+    constructor() {
+        this.name = "Clay";
+    }
+    getGreeting() {
+        return `Hi my name is ${this.name}.`;
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax.getGreeting());
+
+
+class NewSyntax {
+    name = "C-man";
+    getGreeting = () => {
+        return `Hi my name is ${this.name}.`;
+    }
+}
+
+const newSyntax = new NewSyntax();
+const news = newSyntax.getGreeting();
+console.log(news);
