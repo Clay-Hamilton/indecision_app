377ca8a93c998c8f1959b07b2e07e4c2795b0f5c
Clay Hamilton <clayham@gmail.com>
2022-01-27 19:04:22 -0800

    -installed/set up React-Modal - replaced default JS alert function -streamlined stateless functional components to implicitly return JSX

--- src/app.js
+++ src/app.js
@@ -1,31 +1,7 @@
 //imported files run before app.js:
+import { proposalPlugins } from "@babel/preset-env/data/shipped-proposals";
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
-
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
-
-class OldSyntax {
-    constructor() {
-        this.name = "Clay";
-    }
-    getGreeting() {
-        return `Hi my name is ${this.name}.`;
-    }
-}
-
-const oldSyntax = new OldSyntax();
-console.log(oldSyntax.getGreeting());
-
-
-class NewSyntax {
-    name = "C-man";
-    getGreeting = () => {
-        return `Hi my name is ${this.name}.`;
-    }
-}
-
-const newSyntax = new NewSyntax();
-const news = newSyntax.getGreeting();
-console.log(news);
+ReactDOM.render(<IndecisionApp/>, document.getElementById("app"));
